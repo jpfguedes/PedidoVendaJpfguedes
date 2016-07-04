a279539c8eeafbf41ca32a7c320600e539ae81a1
Joao Paulo Figueiredo Guedes <jpfguedes@gmail.com>
2016-07-04 18:10:26 -0400

    Configuração do log4j; Implementação do Managed Bean CadastroProdutoBean.
    (En-US): Log4j add to project and its configuration;
    Managed Bean CadastroProdutoBean's implementation.

---
+++ PedidoVenda/src/main/java/com/jpfguedes/pedidovenda/controller/CadastroProdutoBean.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.jpfguedes.pedidovenda.controller;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+/**
+ * @author joao.guedes
+ *
+ */
+
+@Named
+@RequestScoped
+public class CadastroProdutoBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	
+	public void salvar() {
+		
+	}
+}
--- PedidoVenda/src/main/java/com/jpfguedes/pedidovenda/util/jsf/JsfExceptionHandler.java
+++ PedidoVenda/src/main/java/com/jpfguedes/pedidovenda/util/jsf/JsfExceptionHandler.java
@@ -15,6 +15,9 @@ import javax.faces.context.FacesContext;
 import javax.faces.event.ExceptionQueuedEvent;
 import javax.faces.event.ExceptionQueuedEventContext;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import com.jpfguedes.pedidovenda.service.NegocioException;
 
 /**
@@ -22,6 +25,8 @@ import com.jpfguedes.pedidovenda.service.NegocioException;
  *
  */
 public class JsfExceptionHandler extends ExceptionHandlerWrapper {
+	
+	private static Log log = LogFactory.getLog(JsfExceptionHandler.class);
 	
 	private ExceptionHandler wrapped;
 	
@@ -59,6 +64,7 @@ public class JsfExceptionHandler extends ExceptionHandlerWrapper {
 					FacesUtil.addErrorMessage(negocioException.getMessage());
 				} else {
 					handled = true;
+					log.error("Erro de sistema: " + exception.getMessage(), exception);
 					redirect("/Erro.xhtml");
 				}
 			} finally {
