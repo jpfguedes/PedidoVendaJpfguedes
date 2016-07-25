a5f1a2d4a0b18e48ca60cc65b678fc64ccd4fe6e
joao.guedes <joao.guedes@p24dt6371.mpt.intra>
2016-07-25 18:06:18 -0400

    (Pt-BR): 1- Implementação de um produtor de EntityManagers 		(EntityManagerProducer.java);
     		 2- Implementação do repositório de categorias (Categorias.java)
    
    ----------------------------------------------
    
    (En-US): 1- A EntityManager's producer's implementation
    (EntityManagerProducer.java);
    
    		 2- Categoria's repository's implementation (Categorias.java).

--- PedidoVenda/src/main/java/com/jpfguedes/pedidovenda/controller/CadastroProdutoBean.java
+++ PedidoVenda/src/main/java/com/jpfguedes/pedidovenda/controller/CadastroProdutoBean.java
@@ -4,11 +4,15 @@
 package com.jpfguedes.pedidovenda.controller;
 
 import java.io.Serializable;
+import java.util.List;
 
-import javax.faces.bean.ViewScoped;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
+import com.jpfguedes.pedidovenda.model.Categoria;
 import com.jpfguedes.pedidovenda.model.Produto;
+import com.jpfguedes.pedidovenda.repository.Categorias;
 
 /**
  * @author joao.guedes
@@ -20,15 +24,25 @@ import com.jpfguedes.pedidovenda.model.Produto;
 public class CadastroProdutoBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	
+	@Inject
+	private Categorias categorias;
 
 	private Produto produto;
-	
+
+	private List<Categoria> categoriasRaizes;
+
 	public CadastroProdutoBean() {
 		produto = new Produto();
 	}
 
+	public void inicializar() {
+		System.out.println("Inicializando...");
+		categoriasRaizes = categorias.raizes();
+	}
+
 	public void salvar() {
-		
+
 	}
 
 	/**
@@ -36,6 +50,21 @@ public class CadastroProdutoBean implements Serializable {
 	 */
 	public Produto getProduto() {
 		return produto;
+	}
+
+	/**
+	 * @return the categoriasRaizes
+	 */
+	public List<Categoria> getCategoriasRaizes() {
+		return categoriasRaizes;
+	}
+
+	/**
+	 * @param categoriasRaizes
+	 *            the categoriasRaizes to set
+	 */
+	public void setCategoriasRaizes(List<Categoria> categoriasRaizes) {
+		this.categoriasRaizes = categoriasRaizes;
 	}
 
 }
